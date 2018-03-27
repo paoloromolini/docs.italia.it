d8ee97dc45f01b2823acd1e6ef776039d17dde81
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2018-03-27 16:56:51 +0200

    WIP: publisher model

---
+++ readthedocs/docsitalia/models.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+
+from readthedocs.projects.models import Project
+from readthedocs.oauth.models import RemoteOrganization
+
+
+@python_2_unicode_compatible
+class Publisher(models.Model):
+    """
+    The Publisher is the organization that hosts projects
+
+    The idea is to tie a Publisher to a RemoteOrganization, if we have a
+    Publisher instance for a RemoteOrganization we can sync its data as
+    available in the well-known repo and config files.
+
+    Given the requirement of handling content in different languages we don't
+    want to duplicate the publisher data here so the config file is the source
+    of truth. A parsed version of the configuration is saved in the metadata
+    field.
+
+    The publisher and the project homepage are handled by a django view.
+    """
+    # Auto fields                                                                                        
+    pub_date = models.DateTimeField(_('Publication date'), auto_now_add=True)
+    modified_date = models.DateTimeField(_('Modified date'), auto_now=True)
+
+    # we need something unique
+    name = models.CharField(_('Name'), max_length=255, unique=True)
+    slug = models.SlugField(_('slug'), max_length=255, unique=True)
+
+    # TODO: is this enough to hold the publisher metadata?
+    # https://github.com/italia/docs-italia-starter-kit/tree/master/repo-configurazione
+    metadata = models.JSONField(_('Metadata'), blank=True)
+    # the name of the repository that will hold the metadata
+    config_repo_name = models.CharField(_('Docs italia config repo'), default=u'docs-italia-conf')
+
+    # the projects linked to the organization, should be the one in the metadata
+    projects = models.ManyToManyField(_('Projects'), Project)
+    # the same publisher may have projects in multiple platforms
+    remote_organization = models.ManyToMany(_('Remote organization'), RemoteOrganization)
+
+    active = models.BooleanField(_('Active'), default=False)
+
+    def __str__(self):
+        return self.name
+
+    # TODO
+    def repos_whitelist(self):
+        return []
+
+    # TODO
+    def parse_metadata_repo(self, metadata_repo):
+        return """{}"""
---
+++ readthedocs/docsitalia/oauth/services/github.py
@@ -0,0 +1,167 @@
+from __future__ import absolute_import
+from builtins import str
+import logging
+import json
+import re
+
+from django.conf import settings
+
+from readthedocs.oauth.services.github import GitHubService
+from readthedocs.oauth.models import RemoteOrganization, RemoteRepository
+from readthedocs.restapi.client import api
+
+from ..models import Publisher
+
+log = logging.getLogger(__name__)
+
+
+class DocsItaliaGithubService(GitHubService):
+    def sync(self):
+        """Sync organizations."""
+        self.sync_organizations()
+
+    def sync_organizations(self):
+        """Sync organizations from GitHub API."""
+        try:
+            orgs = self.paginate('https://api.github.com/user/orgs')
+            for org in orgs:
+                org_resp = self.get_session().get(org['url'])
+                org_obj = self.create_organization(org_resp.json())
+
+                # we ingest only whitelisted organizations
+                if not org_obj:
+                    continue
+
+                publisher = Publisher.objects.get(organizations=org_obj, active=True)
+                metadata_repo = self.paginate(
+                    'https://api.github.com/repos/{org_login}/{docs_italia_conf}'.format(
+                        org_login=org_object.slug,
+                        docs_italia_conf=publisher.config_repo_name
+                    )
+                )
+
+                # no metadata no party
+                if not metadata_repo:
+                    log.debug('Syncing GitHub organizations: no metadata repo for {}'.format(publisher))
+                    continue
+
+                metadata = publisher.parse_metadata_repo(metadata_repo)
+                publisher.metadata = metadata
+                publisher.save()
+
+                whitelisted_repos = publisher.repos_whitelist()
+                # Add repos
+                # TODO ?per_page=100
+                org_repos = self.paginate(
+                    '{org_url}/repos'.format(org_url=org['url'])
+                )
+
+                for repo in org_repos:
+                    if repo['name'] not in whitelisted_repos:
+                        continue
+                    self.create_repository(repo, organization=org_obj)
+
+                # TODO: set active=False for projects that are not whitelisted anymore
+        except (TypeError, ValueError) as e:
+            log.error('Error syncing GitHub organizations: %s',
+                      str(e), exc_info=True)
+            raise Exception('Could not sync your GitHub organizations, '
+                            'try reconnecting your account')
+
+    def create_organization(self, fields):
+        """
+        Update or create remote organization from GitHub API response.
+
+        :param fields: dictionary response of data from API
+        :rtype: Publisher
+        """
+        # TODO: is this the right one?
+        name = fields.get('name')
+        try:
+             publisher = Publisher.objects.get(
+                 name=name,
+                 active=True)
+        except Publisher.DoesNotExist:
+             return None
+
+        try:
+            organization = RemoteOrganization.objects.get(
+                slug=fields.get('login'),
+                users=self.user,
+                account=self.account,
+            )
+        except RemoteOrganization.DoesNotExist:
+            # TODO: fun fact: slug is not unique
+            organization = RemoteOrganization.objects.create(
+                slug=fields.get('login'),
+                account=self.account,
+            )
+            organization.users.add(self.user)
+
+        publisher.organizations.add(organization)
+
+        organization.url = fields.get('html_url')
+        organization.name = name
+        organization.email = fields.get('email')
+        organization.avatar_url = fields.get('avatar_url')
+        if not organization.avatar_url:
+            organization.avatar_url = self.default_org_avatar_url
+        organization.json = json.dumps(fields)
+        organization.account = self.account
+        organization.save()
+
+        return organization
+
+    def create_repository(self, fields, privacy=None, organization=None):
+        """                                                                                              
+        Update or create a repository from GitHub API response.
+
+        :param fields: dictionary of response data from API
+        :param privacy: privacy level to support
+        :param organization: remote organization to associate with
+        :type organization: RemoteOrganization
+        :rtype: RemoteRepository
+        """
+        # FIXME: find where projects are created from this
+        privacy = privacy or settings.DEFAULT_PRIVACY_LEVEL
+        if ((privacy == 'private') or
+            (fields['private'] is False and privacy == 'public')):
+            try:
+                repo = RemoteRepository.objects.get(
+                    full_name=fields['full_name'],
+                    users=self.user,
+                    account=self.account,
+                )
+            except RemoteRepository.DoesNotExist:
+                repo = RemoteRepository.objects.create(
+                    full_name=fields['full_name'],
+                    account=self.account,
+                )
+                repo.users.add(self.user)
+            if repo.organization and repo.organization != organization:
+                log.debug('Not importing %s because mismatched orgs',
+                          fields['name'])
+                return None
+            else:
+                repo.organization = organization
+            repo.name = fields['name']
+            repo.description = fields['description']
+            repo.ssh_url = fields['ssh_url']
+            repo.html_url = fields['html_url']
+            repo.private = fields['private']
+            if repo.private:
+                repo.clone_url = fields['ssh_url']
+            else:
+                repo.clone_url = fields['clone_url']
+            repo.admin = fields.get('permissions', {}).get('admin', False)
+            repo.vcs = 'git'
+            repo.account = self.account
+            repo.avatar_url = fields.get('owner', {}).get('avatar_url')
+            if not repo.avatar_url:
+                repo.avatar_url = self.default_user_avatar_url
+            repo.json = json.dumps(fields)
+            repo.save()
+            return repo
+        else:
+            log.debug('Not importing %s because mismatched type',
+                      fields['name'])
